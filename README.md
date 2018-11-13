cc23cbeb061289b96ff678e7f8f9eed32ae79fa1
Byran Wills-Heath <byran@adgico.co.uk>
2018-11-13 23:41:27 +0000

    Update files to new format

---
+++ README.md
@@ -0,0 +1,3 @@
+# C (gcc), Unity
+
+![Build Status](https://travis-ci.org/cyber-dojo-languages/clangplusplus-googlemock.svg?branch=master)
---
+++ start_point/hiker.tests.c
@@ -0,0 +1,11 @@
+#include "hiker.h"
+
+#include <stdio.h>
+
+int main()
+{
+	if(answer() == 42)
+		printf("\n%s\n", "OK");
+	else
+		printf("\n%s\n", "FAIL");
+}
